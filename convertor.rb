a3b06f5a16642164086e1c8b8181ad96c19c6501
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-08-26 16:55:39 +0300

    #5 Add method toMeters (file: convertor.rb).

---
+++ convertor.rb
@@ -0,0 +1,13 @@
+module Convertor
+  def toMeters(height)
+    parts = height.to_s.split(".")
+    intPart = parts[0].to_f
+    fltPart = parts[1].to_f
+
+    intPart *= 30.48
+    fltPart *= 2.54
+
+    fullHeight = intPart + fltPart
+    fullHeight
+  end
+end
