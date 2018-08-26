0950e22a267841a91245d4a2ce98f7f7494ca05c
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-08-26 14:18:00 +0300

    #1 Take from user height, weight units of measurement (file: main.rb).

---
+++ main.rb
@@ -0,0 +1,8 @@
+print 'Enter units of units of measurement (m - metric, s - standard): '
+units = gets.chomp.to_s
+
+print 'Enter your height: '
+height = gets.chomp.to_i
+
+print 'Enter your weight: '
+weight = gets.chomp.to_i
