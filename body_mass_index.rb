94ac66f6c2c320e602b8f86807cda15a857dd392
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-08-26 15:39:44 +0300

    #3 Add calculate method (file: body_mass_index.rb).

--- body_mass_index.rb
+++ body_mass_index.rb
@@ -4,4 +4,15 @@ class BodyMassIndex
     @width = width
     @units = units
   end
+
+  def calculate
+    if @units.equal?("s")
+      @height_in_meters = toMeters(@height)
+      @width_in_kilos = toKilos(@width)
+
+      @bmi = @width_in_kilos / (@height_in_meters ** 2)
+    else
+      @bmi = @width / (@height ** 2)
+    end
+  end
 end
