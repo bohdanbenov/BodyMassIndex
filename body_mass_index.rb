ac1f3a321e8b019db06f177ad70847d5fda182fe
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-08-26 15:59:50 +0300

    #3 Add printScore method (file: body_mass_index.rb).

--- body_mass_index.rb
+++ body_mass_index.rb
@@ -15,4 +15,16 @@ class BodyMassIndex
       @bmi = @width / (@height ** 2)
     end
   end
+
+  def printScore
+    if @bmi < 18.5
+      puts "Your BMI is #{@bmi} what is lower than 18.5 and suggests the person is underweight"
+    elsif @bmi > 18.5 and @bmi < 25
+      puts "Your BMI is #{@bmi} what indicate optimal weight"
+    elsif @bmi > 25 and @bmi < 30
+      puts "Your BMI is #{@bmi} what indicate the person is overweight"
+    else
+      puts "Your BMI is #{@bmi} and suggests the person is obese"
+    end
+  end
 end
